5ae067aedffc375875ae2ec545872a52ae31162a
moFuhidy <mo7ammed9290@gmail.com>
2023-06-27 17:02:54 +0300

    Add the like feature
    fixed the design

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import './style.css';
-
-import { getPokemonData } from './modules/apiOerations.js';
 import displayPokemons from './modules/display.js';
 
-getPokemonData();
-
-window.onload = displayPokemons();
+displayPokemons();
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,24 +1,38 @@
-import { getPokemonData, getPokemonLikes } from './apiOerations.js';
+import { getPokemonData, getPokemonLikes, postPokemonLikes } from './apiOerations.js';
+import { likesAPi } from './apiLinks.js';
 
 const container = document.querySelector('.pokemons-container');
 
 const getPokemon = async (result) => {
   // get the likes of the items before add more likes
   const likes = await getPokemonLikes();
-  const slicedResult = result.slice(0, 9);
-  slicedResult.map(async (item, index) => {
+  const slicedResult = result.slice(1, 10);
+  // sort by name
+  const ordered = slicedResult.sort((a, b) => a.name.localeCompare(b.name));
+  ordered.map(async (item) => {
     const result = await fetch(item.url);
     const pokemon = await result.json();
-    const likeIndex = likes.findIndex((like) => Number(like.item_id) === index);
+
+    const likeIndex = likes.findIndex((like) => {
+      const itemId = like.item_id;
+      if (typeof itemId === 'string') {
+      // The item_id is a string
+        return Number(like.item_id) === pokemon.id;
+      }
+      // Ignore or handle the item differently
+      return false;
+    });
+
     let likeCount;
     if (likeIndex >= 0) {
       likeCount = likes[likeIndex].likes;
     } else {
       likeCount = 0;
     }
-    const card = `<div class="card-wrapper" id='${index}'>
-        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
-        <div class="pokemon-title"> <h3>${pokemon.name}</h3> <i class="fa-regular fa-heart likeBtn" id="${index}"></i></i>
+
+    const card = `<div class="card-wrapper" id='${pokemon.id}'>
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name} image">
+        <div class="pokemon-title"> <h3>${pokemon.name}</h3> <i class="fa-regular fa-heart likeBtn" id="${pokemon.id}"></i></i>
         </div>
             <div class="likes-contain"><h4 class="itemLikes">${likeCount}</h4><p>likes</p></div>
         <div>
@@ -26,6 +40,19 @@ const getPokemon = async (result) => {
         </div>
       </div>`;
     container.innerHTML += card;
+
+    // add feature like functionality
+    const likeBtns = document.querySelectorAll('.likeBtn');
+    const likeCountNums = document.querySelectorAll('.itemLikes');
+
+    likeBtns.forEach((btn, index) => {
+      btn.addEventListener('click', async (e) => {
+        await postPokemonLikes(likesAPi, e.target.id);
+        let likeCount = Number(likeCountNums[index].textContent);
+        likeCount += 1;
+        likeCountNums[index].textContent = likeCount.toString();
+      });
+    });
   });
 };
 
