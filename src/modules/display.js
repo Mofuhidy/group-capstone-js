0b2a071ec53dfad72b838e01e52eea3ffb574616
moFuhidy <mo7ammed9290@gmail.com>
2023-06-27 12:43:13 +0300

    Add the display items functionality

--- src/modules/display.js
+++ src/modules/display.js
@@ -2,20 +2,28 @@ import getPokemonData from './apiOerations.js';
 
 const container = document.querySelector('.pokemons-container');
 
-const displayPokemons = async () => {
-  const data = await getPokemonData();
+const getPokemon = async (result) => {
+  const slicedResult = result.slice(0, 9);
+  slicedResult.map(async (item) => {
+    const result = await fetch(item.url);
+    const pokemon = await result.json();
 
-  data.results.forEach((pokemon) => {
-    const card = `<div class="card-wrapper">
-        <img src="../assets/pngegg.png" alt="">
-        <div class="pokemon-title"> ${pokemon.name} <i class="fa-regular fa-heart"></i>
+    const card = `<div class="card-wrapper" data-id='${pokemon.id}'>
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="">
+        <div class="pokemon-title"> <h3>${pokemon.name}</h3> <i class="fa-regular fa-heart"></i></i>
         </div>
-        <div class="btn">
-          <button type="button">Comment</button>
+            <div class="likes-contain"><p>likes</p></div>
+        <div>
+          <button type="button" class="btn">Comment</button>
         </div>
       </div>`;
     container.innerHTML += card;
   });
+};
+
+const displayPokemons = async () => {
+  const data = await getPokemonData();
+  getPokemon(data.results);
 };
 
 export default displayPokemons;
